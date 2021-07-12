7e290bf8c420085586bd2428504760a9b083ca50
kananrahimli <kenanrehimli48@gmail.com>
2021-07-12 14:31:45 +0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <coming-soon></coming-soon>
+    <router-view/>
   </div>
-  <router-view/>
+  
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import ComingSoon from './components/ComingSoon.vue'
+export default {
+  components:{
+    ComingSoon
   }
 }
+</script>
+<style lang="scss">
+
+@import url('https://fonts.googleapis.com/css2?family=Manrope:wght@800&display=swap');
+
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: 'Manrope', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  
+}
+
+body{
+  background:#ffffff;
+}
+
+
 </style>
---
+++ src/components/ComingSoon.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="positon-relative">
+       <img
+          class="round-close-sales position-absolute"
+          src="../assets/svg/round-close-sales.svg"
+          alt=""
+        />
+    <div id="logo" class="">
+      <img src="../assets/svg/touch-express-logo.svg" alt="" />
+    </div>
+
+    <div class="text-container d-flex justify-content-center  ">
+      <div class="d-flex justify-content-center align-items-center flex-column">
+        <h3>Tezliklə</h3>
+        <p>Lorem Ipsum is simply dummy</p>
+        <img src="../assets/svg/daire.svg" alt="" />
+      </div>
+      
+    </div>
+   
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+#logo {
+  img {
+    max-width: 224px;
+    height: 71px;
+    margin-top: 21px;
+    margin-left: 42px;
+  }
+}
+
+.round-close-sales{
+    right: 0px;
+    max-width: 1000px;
+}
+
+.text-container {
+  background-color: rgba(50, 50, 68, 1);
+  max-width: 930px;
+  height: 272px;
+  margin-top: 130px;
+  margin-left: 42px;
+  border-radius: 40px;
+  border-top-right-radius: 0px;
+  position: relative;
+
+  img{
+    max-width: 118px;
+    height: 118px;
+    position: absolute;
+    top: -56px;
+    left: 100px;
+  }
+  h3 {
+    color: rgba(253, 99, 31, 1);
+    font-weight: 800;
+    font-size: 62px;
+  }
+  p {
+    font-weight: 200 !important;
+    font-size: 20px;
+    color: #fff;
+  }
+}
+</style>
